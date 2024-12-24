8edc2aca97fa732c880f049307a4423141175f77
Ahsa21 <ahmadsalouka@gmail.com>
2024-12-24 10:47:22 -0800

    Initial Commit

---
+++ fifth training/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fifth training
+//
+//  Created by Ahmad Saloukha on 2024-12-24.
+// ffffff
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fifth training/fifth_trainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifth_trainingApp.swift
+//  fifth training
+//
+//  Created by Ahmad Saloukha on 2024-12-24.
+//
+
+import SwiftUI
+
+@main
+struct fifth_trainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
